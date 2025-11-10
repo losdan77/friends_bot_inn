e10a1cebfcf4c3cad38d5db693a8e18219a6f9f8
losdan77 <losdan322@gmail.com>
2025-11-10 23:49:43 +0300

    start deafult message

--- core/handlers/parse.py
+++ core/handlers/parse.py
@@ -22,7 +22,7 @@ async def parse_by_inn(messages: Message, bot: Bot):
     await messages.answer(f'{messages.from_user.first_name} подождите немного! Ищем данные по ИНН:{messages.text}')
 
     try:
-        logger.info('Начало третьего сайта')
+        logger.info('Начало парсинга публичного бизнеса')
         
         async with async_playwright() as p:
             # Выбираем рандомный User-Agent
@@ -49,26 +49,56 @@ async def parse_by_inn(messages: Message, bot: Bot):
             await page.screenshot(path=f'./result/screenshot/third_site/{messages.text}(1).png')
             
             # Кликаем на организацию
-            name_organization = page.locator("//div[contains(@class, 'pb-card__title')]")
+            name_organization = page.locator("//div[contains(@class, 'pb-card__title')]").first
             await name_organization.click()
             
             await page.wait_for_timeout(5000)
-            
-            # Скроллим и берём скриншоты
-            result_info = page.locator("//span[contains(text(), 'Сведения о лице, имеющем право без доверенности действовать от имени юридического лица')]")
-            await result_info.scroll_into_view_if_needed()
             await page.screenshot(path=f'./result/screenshot/third_site/{messages.text}(2).png')
             
-            await page.wait_for_timeout(2000)
-            
-            result_info = page.locator("//span[contains(text(), 'Сведения о непредставлении налоговой отчетности более года')]")
-            await result_info.scroll_into_view_if_needed()
-            await page.screenshot(path=f'./result/screenshot/third_site/{messages.text}(3).png')
-            
-            logger.info('Конец третьего сайта')
+            name_organization_text = await page.locator("//span[contains(@class, 'pb-company-name')]").first.text_content()
+            # inn_organization_text = str(page.locator("//a[contains(@data-appeal-kind, 'EGRUL_INN')]"))
+            inn_organization_text = '7728786311'
+
+            logger.info('Конец парсинга публичного бизнеса')
             
             await context.close()
             await browser.close()
+
+            await messages.answer(f'{messages.from_user.first_name}, данные по ИНН:{messages.text} успешно найдены!')
             
+            await messages.answer(f'''<b>{name_organization_text}</b>
+
+<b>ОБЩАЯ ИНФОРМАЦИЯ:</b>
+ИНН: {inn_organization_text}
+Возраст: 11 лет (регистрация 12.10.2011)
+Адрес: 121596 г. Москва, Ул. Горбунова, Д. 2, Корп Стр. 3,  Кв Этаж/помещ 8/ii Ком./офис 38д/222а 
+Уставный капитал: 10000 руб.
+Основной вид деятельности: 41.20, Строительство жилых и нежилых зданий
+Регистратор: Межрайонная инспекция Федеральной налоговой службы № 46 по г. Москве 
+
+<b>ОРГАНЫ УПРАВЛЕНИЯ:</b>
+<b>Генеральный директор:</b>
+Русак Тимофей Викторович
+(ИНН	773315864324) период с 01.04.2014
+<b>Учредители:</b>
+Аль-тамими Самир Алиевич (10000 руб.) 
+(ИНН /772458576001) <b>(Чистая прибыль от бизнеса за три года ~ 45 млн. руб.)</b>
+
+<b>ФИНАНСОВАЯ ИНФОРМАЦИЯ:</b>
+Среднесписочная численность:~ 115 чел.
+Выручка от продажи за 2021: 179 559 000 ₽
+Чистая прибыль за 2021: -9 100 000 ₽
+Чистая прибыль за последние три года: ~ +20 000 000 ₽
+Исполнительное производство: 
+Как ответчик 0 руб.
+Как истец 0 руб
+
+Последние изменения были совершены 15.02.2022
+Данные актуальны на 17.08.2022
+''', parse_mode='HTML')
+
     except Exception as e:
-        logger.error(f'Ошибка в третьем сайте: {e}')
+        logger.error(f'Ошибка в парсинге публичного бизнеса: {e}')
+        await messages.answer(f'{messages.from_user.first_name}, ошибка в поиске данных по ИНН:{messages.text}')
+
+    
